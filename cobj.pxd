5609e37072f55bf249dcc594c6feeb2fcaa5df7e
Yasuhito FUTATSUKI <futatuki@yf.bsdclub.org>
2014-12-30 05:20:56 +0000

    Revise wrapper management data structure and architecture * Unbound wrapper class instance cannot exist in Python context,   It can be in Cython context, to bind pointer value brought by C function. * Wrapper class constructer on Python context perfomes memory allocation   for pointing entities(elements), to set values and pass to C function. * Generate wrapper class instance for each element of memory sequence   when it is needed. (Don't generate them in advance) As a result, alloc_entity(), dealloc_entity(), unbound() are removed.

--- cobj.pxd
+++ cobj.pxd
@@ -3,18 +3,19 @@ from libc.stddef cimport size_t
 
 cdef class CObjPtr(object):
     cdef void *_c_ptr
-    cdef public const char *_c_base_type
-    cdef public size_t _c_esize
-    cdef readonly int boundstate
+    cdef readonly int _is_const
+    cdef int _is_init
+    cdef readonly int _has_entity
+    cdef readonly const char *_c_base_type
+    cdef readonly size_t _c_esize
     cdef readonly object entity_obj
     cdef public int nelms
-    cdef public list elmlist
     cdef dict __mdict__
     cdef dict __mddict__
-    cdef _allocator(self, int n)
+    cdef void * _allocator(self, int n)
     cdef _deallocator(self)
-    cdef void bind(self, void *ptr, int n=?, object entity_obj=?)
-    cpdef unbind(self)
+    cdef void bind(self, void *ptr, int n=?,
+                         object entity_obj=?, int flg_keep=?)
 
 cdef class CPtrPtr(CObjPtr):
     cdef type ptr_class 
--- cobj.pyx
+++ cobj.pyx
@@ -9,169 +9,43 @@ ELSE:
     from lib.string cimport memset
 from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
 
-_boundstate_unbound      = 0
-_boundstate_bound        = 1
-_boundstate_selfallocate = 2
-
 cdef class CObjPtr(object):
 #    cdef void *_c_ptr
-#    cdef public const char *_c_base_type
-#    cdef public size_t _c_esize
-#    cdef public int boundstate
+#    cdef readonly int _is_const
+#    cdef int _is_init
+#    cdef readonly int _has_entity
+#    cdef readonly const char *_c_base_type
+#    cdef readonly size_t _c_esize
+#    cdef readonly object entity_obj
 #    cdef public int nelms
-#    cdef object elmlist
-#    cdef object __mddict__
-#    cdef object __mdict__
-    def __cinit__(self, nelms=1, vals=None, **m):
+#    cdef dict __mdict__
+#    cdef dict __mddict__
+    def __cinit__(self, int nelms=1, vals=None, int is_const=False, **m):
         self._c_ptr = NULL
-        self.boundstate = _boundstate_unbound
+        self._is_const = is_const
+        self._is_init = False
+        self._has_entity = False
         self.entity_obj = None
         self.nelms  = 0
-        self.elmlist = []
-        self._c_base_type = 'void'
-        self._c_esize = 0
+        if is_const:
+            self._c_base_type = 'void'
+        else:
+            self._c_base_type = 'const void'
+        self._c_esize = 1
         self.__mddict__ = {}
         self.__mdict__ = {}
-    def __init__(self, nelms=1, vals=None, **m):
-        if nelms > 0:
-            self.alloc_entity(nelms, vals, **m)
-    def __getitem__(self, index):
-        if self.boundstate == _boundstate_unbound:
-            raise TypeError('Not bound yet')
-        return self.elmlist[index]
-    def __setitem__(self, index, val):
-        cdef CObjPtr ref
-        cdef CObjPtr ref2
-        if self.boundstate == _boundstate_unbound:
-            raise TypeError('Not bound yet')
-        ref = self.elmlist[index]
-        if val is None:
-           for k in self.__mddict__:
-              ref.__setattr__(k, self.__mddict__[k])
-        elif isinstance(val, dict):
-            for k in val:
-                if not self.__mddict__.has_key(k):
-                    raise TypeError('Unknown member %s' % k)
-            for k in val:
-                ref.__setattr__(k, val[k])
-        elif isinstance(val, self.__class__):
-            ref2 = val
-            for k in ref2.__mddict__:
-                ref.__setattr__(k, ref2.__getattr__(k))
-        else:
-            raise TypeError(
-                'val must be dict of member or instance of %s class'
-                % self.__class__.__name__)
-    def __len__(self):
-        if self.boundstate == _boundstate_unbound:
-            raise TypeError('Not bound yet')
-        if self.nelms == 0:
-            raise TypeError('length unknown')
-        return self.nelms
-    def __next__(self):
-        if self.boundstate == _boundstate_unbound:
-            raise TypeError('Not bound yet')
-        if self.nelms == 0:
-            raise TypeError(
-                "This object don't know how many element allocated")
-        if self.nelms == 1:
-            raise StopIteration
-        return self.elmlist[1]
-    def __dealloc__(self):
-        if self.boundstate == _boundstate_selfallocate:
-            assert self._c_ptr is not NULL
-            self._deallocator()
-            self.boundstate = _boundstate_unbound
-    cdef _allocator(self, int n):
-        cdef void *tmp_ptr
-        tmp_ptr = PyMem_Malloc(self._c_esize * n)
-        if tmp_ptr is NULL:
-            raise MemoryError()
-        IF (    UNAME_SYSNAME == 'Linux'  or UNAME_SYSNAME == 'FreeBSD'
-             or UNAME_SYSNAME == 'NetBSD' or UNAME_SYSNAME == 'OpenBSD'
-             or UNAME_SYSNAME == 'Darwin' ):
-            bzero(tmp_ptr, self._c_esize * n)
-        ELSE:
-            memset(tmp_ptr, 0, self._c_esize * n)
-        self._c_ptr = tmp_ptr
-    cdef _deallocator(self):
-        PyMem_Free(self._c_ptr)
-    def values(self):
-        if self.boundstate == _boundstate_unbound:
-            raise TypeError('Not bound yet')
-        md = {}
-        for m in self.__mddict__:
-            md[m] = self.__getattr__(m)
-        return md
-    cdef void bind(self, void *ptr, int n=0, entity_obj=None):
-        cdef int i
-        cdef void * tmp_ptr
-        cdef CObjPtr ref
-        if self.boundstate != 0:
-            raise TypeError('must do unbind() before bind()')
-            # self.unbind()
-        if ptr is NULL:
-            raise ValueError('cannot bind NULL pointer')
-        #if self.__class__.__name__ != 'CObjPtr':
-        #    raise NotImplementedError()
-        self._c_ptr = ptr
-        self.entity_obj = entity_obj
-        if n > 0:
-            self.nelms = n
-            del self.elmlist[0:]
-            self.elmlist.append(self)
-            i = 1
-            tmp_ptr = ptr + self._c_esize
-            while i < n:
-                ref = self.__class__(0)
-                ref.bind(tmp_ptr, 0, entity_obj)
-                ref.nelms = n - i
-                self.elmlist.append(ref)
-                tmp_ptr = tmp_ptr + self._c_esize
-                i = i + 1
-            i = 1
-            for ref in self.elmlist[1:]:
-                ref.elmlist[0:] = self.elmlist[i:]
-                i = i + 1
-        else:
-            # there is no way to know how many elements to access safely ...
-            self.nelms = 0
-            del self.elmlist[0:]
-        self.boundstate = _boundstate_bound
-    cpdef unbind(self):
-        if self._c_ptr is not NULL:
-            if self.boundstate == _boundstate_selfallocate:
-                self.dealloc_entity()
-        self._c_ptr = NULL
-        self.entity_obj = None
-        self.__mdict__ = {}
-        self.nelms = 0
-        del self.elmlist[0:]
-        self.boundstate  = _boundstate_unbound
-    def cast(self, type t):
-        cdef CObjPtr ref
-        if self.boundstate == _boundstate_unbound:
-            raise TypeError('Not bound yet')
-        assert self._c_ptr is not NULL
-        if ( t is not CObjPtr and
-                not t in CObjPtr.__subclasses__() ):
-            raise TypeError('cast type must be the CObjPtr or its subclass')
-        ref = t(0)
-        ref.bind(self._c_ptr, 0, self.entity_obj)
-        return ref
-    def alloc_entity(self, nelms=1, vals=None, **m):
+    def __init__(self, nelms=1, vals=None, int is_const=False, **m):
         cdef void *tmp_ptr
         cdef int i
         cdef CObjPtr ref
-        if self.boundstate != _boundstate_unbound:
-            raise TypeError('must do unbind() before alloc_entry()')
+        assert self._c_ptr is NULL
         if nelms < 1:
             raise ValueError(
                 'nelms is number of allocation units, '
                 'so it must greater than 0')
-        self._allocator(nelms)
-        self.bind(self._c_ptr, nelms, self)
-        self.boundstate = _boundstate_selfallocate
+        tmp_ptr = self._allocator(nelms)
+        self.bind(tmp_ptr, nelms, None, True)
+        self._has_entity = True
         # initialize allocated entity with values specified by arguments.
         #    1. argument m holds default values
         #    2. vals holds list of values, each of element is a dict of
@@ -205,66 +79,186 @@ cdef class CObjPtr(object):
                     i = i + 1
             else:
                 raise TypeError('vals must be lists of member dict')
+            self._is_init = True 
         except Exception,e:
-            self.dealloc_entity()
-            raise e
-        #
-    def dealloc_entity(self):
-        if self.boundstate == _boundstate_selfallocate:
             self._deallocator()
-            self.boundstate = _boundstate_unbound
             self._c_ptr = NULL
             self.nelms  = 0
-            del self.elmlist[0:]
+            self._is_init = False
+            self._has_entity = False
+            self.entity_obj = None
+            raise e
+        #
+    #
+    def __getitem__(self, index):
+        cdef int i
+        cdef void * tmpptr
+        cdef CObjPtr ref
+        assert self._c_ptr is not NULL
+        i = index if index >= 0 else self.nelms + index
+        if i < 0 or i >= self.nelms:
+            raise IndexError('array index out of range')
+        tmpptr = self._c_ptr + self._c_esize * i
+        ref = self.__class__.__new__(self.__class__, is_const = self._is_const)
+        ref.bind(tmpptr, self.nelms - i,
+                    self if self._has_entity else self.entity_obj)
+        return ref
+    def __setitem__(self, index, val):
+        cdef CObjPtr ref
+        cdef CObjPtr ref2
+        assert self._c_ptr is not NULL
+        if self._is_const and self._is_init:
+            raise TypeError('Pointer points const value. Cannot alter')
+        ref = self[index]
+        ref._is_const = False
+        if val is None:
+           for k in self.__mddict__:
+              ref.__setattr__(k, self.__mddict__[k])
+        elif isinstance(val, dict):
+            for k in val:
+                if not self.__mddict__.has_key(k):
+                    raise TypeError('Unknown member %s' % k)
+            for k in val:
+                ref.__setattr__(k, val[k])
+        elif isinstance(val, self.__class__):
+            ref2 = val
+            for k in ref2.__mddict__:
+                ref.__setattr__(k, ref2.__getattr__(k))
+        else:
+            raise TypeError(
+                'val must be dict of member or instance of %s class'
+                % self.__class__.__name__)
+    def __len__(self):
+        assert self._c_ptr is not NULL
+        if self.nelms == 0:
+            raise TypeError('length unknown')
+        return self.nelms
+    def __next__(self):
+        assert self._c_ptr is not NULL
+        if self.nelms == 0:
+            raise TypeError(
+                "This object don't know how many element allocated")
+        if self.nelms == 1:
+            raise StopIteration
+        return self[1]
+    def __dealloc__(self):
+        if self._has_entity:
+            assert self._c_ptr is not NULL
+            self._deallocator()
+            self._has_entity = False
+    cdef void * _allocator(self, int n):
+        cdef void *tmp_ptr
+        tmp_ptr = PyMem_Malloc(self._c_esize * n)
+        if tmp_ptr is NULL:
+            raise MemoryError()
+        IF (    UNAME_SYSNAME == 'Linux'  or UNAME_SYSNAME == 'FreeBSD'
+             or UNAME_SYSNAME == 'NetBSD' or UNAME_SYSNAME == 'OpenBSD'
+             or UNAME_SYSNAME == 'Darwin' ):
+            bzero(tmp_ptr, self._c_esize * n)
+        ELSE:
+            memset(tmp_ptr, 0, self._c_esize * n)
+        return tmp_ptr
+    cdef _deallocator(self):
+        PyMem_Free(self._c_ptr)
+    def values(self):
+        assert self._c_ptr is not NULL
+        md = {}
+        for m in self.__mddict__:
+            md[m] = self.__getattr__(m)
+        return md
+    cdef void bind(self, void *ptr, int n=0,
+                        entity_obj=None, int flg_keep=False):
+        cdef int i
+        cdef void * tmp_ptr
+        cdef CObjPtr ref
+        assert self._c_ptr is NULL
+        if ptr is NULL:
+            raise ValueError('cannot bind NULL pointer')
+        self._c_ptr = ptr
+        self.entity_obj = entity_obj
+        if n < 0:
+            raise ValueError('number of elements must 0 or positive value')
+        self.nelms = n
+        self._is_init = not flg_keep
+    def cast(self, type t, int is_const=False):
+        cdef CObjPtr ref
+        assert self._c_ptr is not NULL
+        if ( t is not CObjPtr and
+                not t in CObjPtr.__subclasses__() ):
+            raise TypeError('cast type must be the CObjPtr or its subclass')
+        ref = t.__new__(t,is_const=is_const)
+        ref.bind(self._c_ptr, 0,
+                    self if self._has_entity else self.entity_obj)
+        return ref
 
 cdef class CPtrPtr(CObjPtr):
-    def __cinit__(self, nelms=1, vals=None, ptr_class=CObjPtr, **m):
+    def __cinit__(self, int nelms=1, vals=None,
+                        int is_const=False, ptr_class=CObjPtr, **m):
         if ( ptr_class is not CObjPtr and
                 not ptr_class in CObjPtr.__subclasses__() ):
             raise TypeError('ptr_class must be the CObjPtr or its subclass')
         self.ptr_class = CObjPtr
         self._c_base_type = 'void *'
         self._c_esize = sizeof(void *)
-        self.__mddict__ = { 'm_' : None }
-    def __init__(self, nelms=1, vals=None, ptr_class=CObjPtr, **m):
+        self.__mddict__ = { 'p_' : None }
+    def __init__(self, int nelms=1, vals=None,
+                        int is_const=False, ptr_class=CObjPtr, **m):
         if nelms > 0:
             self.alloc_entity(nelms, vals, **m)
-    property m_:
+    property p_:
         def __get__(self):
             cdef void * tmp_ptr
-            cdef CObjPtr m_
-            if self.boundstate == _boundstate_unbound:
-                raise TypeError('Not bound yet')
+            cdef CObjPtr p_
             assert self._c_ptr is not NULL
-            m_ = self.__mdict__.get('m_', None)
+            p_ = self.__mdict__.get('p_', None)
             tmp_ptr = (<void**>(self._c_ptr))[0]
             if tmp_ptr is NULL:
-                self.__mdict__['m_'] = None
+                self.__mdict__['p_'] = None
                 return None
             else:
-                if m_ is None or m_._c_ptr != tmp_ptr:
-                    m_ = self.ptr_class(0)
-                    m_.bind(tmp_ptr)
-                self.__mdict__['m_'] = m_
-                return m_
+                if p_ is None or p_._c_ptr != tmp_ptr:
+                    p_ = self.ptr_class(0)
+                    p_.bind(tmp_ptr)
+                self.__mdict__['p_'] = p_
+                return p_
         def __set__(self,val):
-            if self.boundstate == _boundstate_unbound:
-                raise TypeError('Not bound yet')
             assert self._c_ptr is not NULL
+            if self._is_const and self._is_init:
+                raise TypeError('Pointer points const value. Cannot alter')
             if val is None:
-                self.__mdict__['m_'] = None
+                self.__mdict__['p_'] = None
                 (<void**>(self._c_ptr))[0] = NULL
             elif not isinstance(val,self.ptr_class):
-                raise TypeError('attribute m_ must be a %s instance' %
+                raise TypeError('attribute p_ must be a %s instance' %
                         self.ptr_class.__name__)
-            elif val.boundstate == _boundstate_unbound:
-                raise ValueError('m_ must be a %s bounded instance' %
+            elif not val._is_init:
+                raise ValueError('p_ must be a %s bounded instance' %
                         self.ptr_class.__name__)
             else:
-                self.__mdict__['m_'] = val
+                self.__mdict__['p_'] = val
                 (<void**>(self._c_ptr))[0] = (<CObjPtr>val)._c_ptr
         def __del__(self):
-            if self.boundstate != _boundstate_unbound:
-                assert self._c_ptr is not NULL
-                self.__mdict__['m_'] = None
-                (<void**>(self._c_ptr))[0] = NULL
+            assert self._c_ptr is not NULL
+            self.__mdict__['p_'] = None
+            (<void**>(self._c_ptr))[0] = NULL
+
+#cdef class CCharPtr(CObjPtr):
+#    def __cinit__(self, int nelms=1, vals=None, int is_const=False, **m):
+#        self._c_base_type='char'
+#        self._c_esize = sizeof(char *)
+#        self.__mddict__ = { 'p_' : 0, 's_' : '' }
+#    property p_:
+#        def __get__(self):
+#            assert self._c_ptr is not NULL
+#            return (<char*>(self._c_ptr))[0]
+#        def __del__(self):
+#            assert self._c_ptr is not NULL
+#            (<char *>(self._c_ptr))[0] = 0
+#    property s_:
+#        def __get__(self):
+#            assert self._c_ptr is not NULL
+#            return <char *>(self._c_ptr)
+#        def __del__(self):
+#            assert self._c_ptr is not NULL
+#            (<char *>(self._c_ptr))[0] = 0
+#
