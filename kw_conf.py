4ac7a7cb5db1cfe7a70ae96667cd46266b33e4a9
FUTATSUKI Yasuhito <futatuki@yf.bsdclub.org>
2018-08-17 09:39:50 +0900

    add filter to expand keywords and its configuration file for git

---
+++ kw_conf.py
@@ -0,0 +1,7 @@
+custom_kw = {
+    'MANIFEST.in'           : { 'yfId' : '%H' },
+    'cwrapper/generator.py' : { 'yfId' : '%H' },
+    'cwrapper/cobj.pxd'     : { 'yfId' : '%H' },
+    'cwrapper/cobj.pyx'     : { 'yfId' : '%H' },
+    'primptrgen.py'         : { 'yfId' : '%H' },
+    'setup.py'              : { 'yfId' : '%H' }  }
